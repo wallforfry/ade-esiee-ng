5c27c21dfb6d96a1b3d39f101722e615f48b9001
Wallerand Delevacq <wallforfry@gmail.com>
2018-03-25 15:50:34 +0200

    Add logging to ade-parser

--- ade-parser/ade-parser.py
+++ ade-parser/ade-parser.py
@@ -9,9 +9,9 @@ import urllib.error
 
 import json
 
-import atexit
 from icalendar import Calendar
 import time, threading
+import logging
 
 def download_ics_from_planif():
     url = "https://planif.esiee.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=147,738,739,743,744,2841,5757,746,747,748,2781,2782,3286,682,683,684,685,659,665,674,680,681,727,733,785,998,1295,2555,2743,5215,5688,731,734,735,736,740,741,742,780,782,1852,2584,4350,5321,786,787,788,789,790,2270,2275,2277,2278,2282,704,745,773,775,776,4937,728,2117,772,719,2112,183,185,196,4051,4679,2072,2074,2272,2276,2089,154,713,163,167,700,701,705,707,708,712,714,715,716,724,725,726,737,749,758,759,1057,1858,1908,2090,2108,2281,428,717,720,721,722,2265,2274,2279&projectId=7&calType=ical&nbWeeks=12"
@@ -23,6 +23,7 @@ def download_ics_from_planif():
         with open("data/ADECal.ics", "w") as file:
             file.write(g)
             file.close()
+            logging.info("[ADE-PARSER] ICS file is up to date")
         return True
 
     except urllib.error.URLError as e:
@@ -52,6 +53,7 @@ def ics_to_json_from_ade():
     with open("data/calendar.json", "w") as f:
         json.dump(events_list, f)
         f.close()
+        logging.info("[ADE-PARSER] Json file is up to date")
 
 
 def get_json_calendar():
@@ -69,8 +71,11 @@ def get_json_calendar():
 def update_calendar():
     if download_ics_from_planif():
         ics_to_json_from_ade()
+        logging.info("[ADE-PARSER] Calendar is up to date")
     threading.Timer(600, update_calendar).start()
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+    logging.info("[ADE-PARSER] Starting..")
     update_calendar()
